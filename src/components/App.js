d0266a15f30246736eedce1b6b4dd90002fab43b
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-04-07 10:55:14 +0200

    The first commit

---
+++ src/components/App.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import './App.css';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
+
+class App extends Component {
+
+  counter = 7
+  state = { 
+    tasks:[
+      {
+        id: 0,
+        text: 'Zagrać wreszcie w Wiedźmina 3',
+        date: "2020-05-12",
+        important: true,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 1,
+        text: 'Wypić piwo z kolegami',
+        date: "2020-05-12",
+        important: true,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 2,
+        text: 'Pójść na spacer',
+        date: "2020-05-12",
+        important: false,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 3,
+        text: 'Zjeść makaron',
+        date: "2020-05-12",
+        important: false,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 4,
+        text: 'Zrobić zakupy',
+        date: "2020-05-12",
+        important: false,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 5,
+        text: 'Pójść na rower',
+        date: "2020-05-12",
+        important: true,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 6,
+        text: 'Pospać',
+        date: "2020-05-12",
+        important: true,
+        active: true,
+        finishDate: null
+      }
+    ]
+   }
+
+  deleteTask = (id) => {
+    // console.log('delete' + id)
+    // const tasks = [...this.state.tasks];
+    // console.log(tasks)
+    // const index = tasks.findIndex(task => task.id===id)
+    // tasks.splice(index, 1)
+    // console.log(tasks)
+
+    // this.setState({
+    //   tasks
+    // })
+
+    let tasks = [...this.state.tasks]
+    tasks = tasks.filter(task => task.id!==id)
+    this.setState({
+      tasks
+    })
+  }
+
+  changeTaskStatus = (id) => {
+    console.log('done' + id)
+    const tasks = Array.from(this.state.tasks)
+    tasks.forEach(task => {
+      if(task.id ===id){
+        task.active = false
+        task.finishDate = new Date().getTime()
+      }
+      this.setState({
+        tasks
+      })
+    })
+  }
+  AddTask = (text, date, important) => {
+    const task = {
+        id: this.counter++,
+        text, //tekst z inputa
+        date, //tekst z inputa
+        important, //wartość z inputa
+        active: true,
+        finishDate: null
+    }
+    // this.counter++
+    console.log(task, this.counter)
+    this.setState(prevState => ({
+      tasks: this.state.tasks.concat(task)
+    }))
+    return true
+  }
+
+  render() { 
+    return ( 
+    <div className="App">
+      <h1>To Do App</h1>
+      <AddTask add={this.AddTask}/>
+      <hr/>
+      <TaskList tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus}/>
+    </div> )
+  }
+}
+ 
+export default App;
