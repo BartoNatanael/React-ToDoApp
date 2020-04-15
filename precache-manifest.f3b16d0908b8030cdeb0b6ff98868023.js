1a80007a049ead3f128b8631725d263af7a2dcf5
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-04-15 15:51:56 +0200

    Updates

---
+++ precache-manifest.f3b16d0908b8030cdeb0b6ff98868023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070451afd1938f0ffa444a2b28360846",
+    "url": "/React-ToDoApp/index.html"
+  },
+  {
+    "revision": "a8398159d84557f19a8f",
+    "url": "/React-ToDoApp/static/css/main.d226a93d.chunk.css"
+  },
+  {
+    "revision": "cda01129f1732f16bbb8",
+    "url": "/React-ToDoApp/static/js/2.a6d9148e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoApp/static/js/2.a6d9148e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8398159d84557f19a8f",
+    "url": "/React-ToDoApp/static/js/main.5b18d47a.chunk.js"
+  },
+  {
+    "revision": "343d369daa19f1bccd04",
+    "url": "/React-ToDoApp/static/js/runtime-main.8f284745.js"
+  }
+]);
